6c27938afc1c083064a0acad9b2bce48e8c0b1f5
Artemis Tosini <me@artem.ist>
2024-01-28 06:06:42 +0000

    kernel: add basic kernelEnvironment

--- modules/system/boot/kernel.nix
+++ modules/system/boot/kernel.nix
@@ -1,5 +1,7 @@
 { config, pkgs, lib, ... }:
-with lib; {
+with lib;
+let cfg = config.boot.kernel;
+in {
   options = {
     boot.kernel.enable = mkEnableOption (lib.mdDoc
       "the FreeBSD kernel (sys). This can be disabled for jails where the host kernel is used")
@@ -17,11 +19,11 @@ with lib; {
     };
 
     boot.kernelEnvironment = mkOption {
-      type = with types; attrsOf (oneOf [ str bool int ]);
+      type = types.attrsOf types.str;
       default = { };
       example = {
         boot_serial = "YES";
-        "kern.maxusers" = 16;
+        "kern.maxusers" = "16";
       };
       description = lib.mdDoc ''
         Environment set for kernel, similar to kernel arguments on Linux.
@@ -30,16 +32,13 @@ with lib; {
     };
   };
 
-  config = mkIf config.boot.kernel.enable {
+  config = mkIf config.boot.kernel.enable (let
+    kernelPath = "${cfg.package}/${config.system.boot.loader.kernelFile}";
+    modulesPath = "${cfg.package}/${config.system.boot.loader.modulesPath}";
+  in {
     system.build = { inherit (config.boot) kernel; };
 
-    system.systemBuilderCommands = let
-      kernelPath =
-        "${config.boot.kernel.package}/${config.system.boot.loader.kernelFile}";
-      modulesPath =
-        "${config.boot.kernel.package}/${config.system.boot.loader.modulesPath}";
-      kernelEnvJson = builtins.toJSON config.boot.kernelEnvironment;
-    in ''
+    system.systemBuilderCommands = ''
       if [ ! -f ${kernelPath} ]; then
         echo "The bootloader cannot find the proper kernel image."
         echo "(expecting ${kernelPath})"
@@ -48,8 +47,12 @@ with lib; {
 
       ln -s ${kernelPath} $out/kernel
       ln -s ${modulesPath} $out/kernel-modules
-      echo -n "${kernelEnvJson}" > $out/kernel-env.json
     '';
-  };
+
+    boot.kernelEnvironment = {
+      module_path = modulesPath;
+      init_shell = config.environment.binsh;
+    };
+  });
 }
 
