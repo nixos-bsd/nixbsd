0053c95934d177a09df787b37efc7f62b6ff10ad
Artemis Tosini <me@artem.ist>
2024-01-28 02:58:32 +0000

    configurations/base: don't wait for an IP

--- configurations/base/default.nix
+++ configurations/base/default.nix
@@ -1,7 +1,10 @@
-{ config, ... }: {
+{ config, pkgs, ... }: {
   nixpkgs.hostPlatform = "x86_64-freebsd14";
 
   users.users.root.initialPassword = "toor";
+
+  # Don't make me wait for an address...
+  networking.dhcpcd.wait = "background";
 
   users.users.bestie = {
     isNormalUser = true;
