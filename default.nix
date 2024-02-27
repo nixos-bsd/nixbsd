c2daca09220c66df3f4353c6e77f163c95370b94
Artemis Tosini <me@artem.ist>
2024-02-27 03:55:39 +0000

    default.nix: more closely match NixOS version

--- default.nix
+++ default.nix
@@ -1,5 +1,5 @@
 { configuration ?
-  import <nixpkgs/nixos/lib/from-env.nix> "NIXBSD_CONFIG" <nixbsd-config.nix>
+  import <nixpkgs/nixos/lib/from-env.nix> "NIXOS_CONFIG" <nixbsd-config>
 , system ? builtins.currentSystem }:
 let
   eval = import ./lib/eval-config.nix {
