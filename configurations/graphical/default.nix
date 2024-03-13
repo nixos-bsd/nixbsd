cf0e90e3b6d5fc228b8dc9354528c0f06a24363d
Audrey Dutcher <audrey@rhelmot.io>
2024-03-13 16:00:43 -0700

    Fix xfce config

--- configurations/graphical/default.nix
+++ configurations/graphical/default.nix
@@ -6,6 +6,7 @@
   services.xserver = {
     enable = true;
     displayManager.lightdm.enable = true;
+    displayManager.defaultSession = "xfce";
     desktopManager.xfce = {
       enable = true;
     };
