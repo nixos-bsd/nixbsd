57954bcb2b1976fd7dac20f21734a87505f498c1
Audrey Dutcher <audrey@rhelmot.io>
2024-03-13 20:57:56 -0700

    lightdm: hack: create tmpfiles with correct owner

--- modules/services/x11/display-managers/lightdm.nix
+++ modules/services/x11/display-managers/lightdm.nix
@@ -213,11 +213,8 @@ in
 
     # lightdm relaunches itself via just `lightdm`, so needs to be on the PATH
     rc.services.display-manager.precmds.start = ''
-      mkdir -p /run/lightdm
-      mkdir -p /var/cache/lightdm
-      mkdir -p /var/lib/lightdm
-      mkdir -p /var/lib/lightdm-data
-      mkdir -p /var/log/lightdm
+      mkdir -p /run/lightdm /var/cache/lightdm /var/lib/lightdm /var/lib/lightdm-data /var/log/lightdm
+      chown lightdm:lightdm /run/lightdm /var/cache/lightdm /var/lib/lightdm /var/lib/lightdm-data /var/log/lightdm
       export PATH=${lightdm}/sbin:$PATH
     '';
 
