6c8713160dbb4926df80d95ef22d31dfacd73653
Artemis Tosini <me@artem.ist>
2024-12-04 02:33:49 +0000

    Add design idea for new freebsd rc options

--- modules/module-list-gendered.nix
+++ modules/module-list-gendered.nix
@@ -3,5 +3,6 @@ nixpkgsPath: hostPlatform:
 if hostPlatform.isFreeBSD then [
   ./system/boot/loader/stand
   ./system/boot/init/portable/freebsd.nix
+  ./system/boot/init/freebsd-rc.nix
 ] else
   throw "Unsupported target platform ${hostPlatform.system}"
---
+++ modules/system/boot/init/freebsd-rc.nix
@@ -0,0 +1,198 @@
+{
+  pkgs,
+  lib,
+  ...
+}:
+
+with lib;
+
+let
+  variableName = types.strMatching "[a-zA-Z_][a-zA-Z0-9_]*";
+in
+{
+  options.freebsd.rc = {
+    package = mkOption {
+      type = types.package;
+      default = pkgs.freebsd.rc;
+      description = ''
+        The FreeBSD rc package to use. Should contain `/etc/rc`, `/etc/rc.subr`, etc.
+        See {manpage}`rc(8)`.
+      '';
+    };
+    services = mkOption {
+      default = { };
+      description = "List of services to run. See `{manpage}`rc.subr(8).";
+      type = types.attrsOf (
+        types.submodule (
+          { config, name, ... }:
+          {
+            options = {
+              name = mkOption {
+                type = variableName;
+                description = "Name of the service, also used for rc variables.";
+              };
+
+              description = mkOption {
+                default = "";
+                type = types.singleLineStr;
+                description = "Description of service included in configuration file.";
+              };
+
+              dummy = mkOption {
+                default = false;
+                type = types.bool;
+                description = ''
+                  Whether to create a dummy service with no commands.
+                  This is generally used for targets, like `NETWORKING`.
+                '';
+              };
+
+              path = mkOption {
+                default = [ ];
+                type =
+                  with types;
+                  listOf (oneOf [
+                    package
+                    str
+                  ]);
+                description = ''
+                  Packages to add to the services {env}`PATH` environment variable.
+                '';
+              };
+
+              bsdUtils = mkOption {
+                default = false;
+                type = types.bool;
+                description = ''
+                  Whether to use BSD binaries.
+                  The default (false) is to use GNU coreutils.
+                '';
+              };
+
+              environment = mkOption {
+                default = { };
+                type =
+                  with types;
+                  attrsOf (
+                    nullOr (oneOf [
+                      str
+                      path
+                      package
+                    ])
+                  );
+                description = "Environment variables passed to the service's commands";
+              };
+
+              rcorderSettings = mkOption {
+                description = "Settings used when ordering services with {manpage}`rcorder(8)`.";
+                default = { };
+                type = types.submodule {
+                  freeformType = types.attrsOf (types.listOf variableName);
+                  options = {
+                    PROVIDE = mkOption {
+                      default = [ ];
+                      type = types.listOf variableName;
+                      description = "Requirements provided by this service. Normally this is the service name.";
+                    };
+                    REQUIRE = mkOption {
+                      default = [ ];
+                      type = types.listOf variableName;
+                      description = "Requirements to start the service.";
+                    };
+                    BEFORE = mkOption {
+                      default = [ ];
+                      type = types.listOf variableName;
+                      description = "Services that will be started after this service.";
+                    };
+                    KEYWORD = mkOption {
+                      default = [ ];
+                      type = types.listOf variableName;
+                      description = "Keywords that determine when this service is started, e.g. `nojail`.";
+                    };
+                  };
+                  config = {
+                    PROVIDE = [ name ];
+                  };
+                };
+              };
+
+              shellVariables = mkOption {
+                description = ''
+                  Shell variables to set after sourcing {path}`/etc/rc.subr`.
+                  For a full list see `run_rc_command` under {manpage}`rc.subr(8)`.
+                '';
+                default = { };
+                type = types.submodule {
+                  freeformType = types.attrsOf types.str;
+                  config = {
+                    name = mkOptionDefault name;
+                    rcvar = mkOptionDefault "${name}_enable";
+                  };
+                };
+              };
+
+              namedShellVariables = mkOption {
+                description = ''
+                  Convenience alias for `shellVarables`.
+                  Variable names are prefixed with `$${name}_`.
+                '';
+                default = { };
+                type = types.submodule { freeformType = types.attrsOf types.str; };
+              };
+
+              hooks = mkOption {
+                description = ''
+                  Shell text run when various events happen.
+                  These are embedded in a function and the corresponding varaible is set.
+                  See list under `run_rc_command` in {manpage}`rc.subr(8)`.
+                '';
+                default = { };
+                type = types.submodule {
+                  freeformType = types.attrsOf types.lines;
+
+                  options = {
+                    start_precmd = mkOption {
+                      default = null;
+                      type = types.nullOr types.lines;
+                      description = ''
+                        Shell commands to run before starting the service.
+                      '';
+                    };
+                    start_postcmd = mkOption {
+                      default = null;
+                      type = types.nullOr types.lines;
+                      description = ''
+                        Shell commands to run after starting the service.
+                      '';
+                    };
+                    stop_precmd = mkOption {
+                      default = null;
+                      type = types.nullOr types.lines;
+                      description = ''
+                        Shell commands to run before stopping the service.
+                      '';
+                    };
+                    stop_postcmd = mkOption {
+                      default = null;
+                      type = types.nullOr types.lines;
+                      description = ''
+                        Shell commands to run after stopping the service.
+                      '';
+                    };
+                  };
+                };
+              };
+            };
+
+            config = {
+              name = mkOptionDefault name;
+              shellVariables = mapAttrs' (
+                var: value: nameValuePair "${name}_${var}" value
+              ) config.namedShellVariables;
+            };
+          }
+        )
+      );
+    };
+  };
+}
