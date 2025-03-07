bf8ae8fd1be04ab2f8713b1cfdd49ebb3a113db5
Audrey Dutcher <audrey@rhelmot.io>
2025-03-06 23:27:58 -0700

    use atomic mkdir instead of flock for switch-to-configuration system lock

--- modules/system/activation/switchable-system.nix
+++ modules/system/activation/switchable-system.nix
@@ -29,7 +29,6 @@
         --subst-var-by toplevel ''${!toplevelVar} \
         --subst-var-by coreutils "${pkgs.coreutils}" \
         --subst-var-by diffutils "${pkgs.diffutils}" \
-        --subst-var-by flock "${pkgs.flock}" \
         --subst-var-by rcorder "${pkgs.freebsd.rcorder}" \
         --subst-var-by distroId ${
           lib.escapeShellArg config.system.nixos.distroId
