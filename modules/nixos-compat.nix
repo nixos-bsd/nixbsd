fafb02fc6dbb7640f89749448d8d42344d72518e
Audrey Dutcher <audrey@rhelmot.io>
2026-02-27 23:47:46 -0700

    Add systemd.services stub

--- modules/nixos-compat.nix
+++ modules/nixos-compat.nix
@@ -5,6 +5,11 @@
     default = [];
     description = "This option exists only for compatibility with NixOS modules and does not have any effect.";
   };
+  options.systemd.services = lib.mkOption {
+    type = lib.types.attrsOf lib.types.anything;
+    default = [];
+    description = "This option exists only for compatibility with NixOS modules and does not have any effect.";
+  };
   options.security.sudo-rs = lib.mkOption {
     type = lib.types.attrsOf lib.types.anything;
     default = [];
